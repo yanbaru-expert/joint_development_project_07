697b2a380c42200a4a9a292c7ea50c63a7730901
Tako <takoforme@gmail.com>
2020-04-20 10:59:26 +0900

    editとupdate実装&マイグレーションファイルの型を修正

--- db/migrate/20200417022620_create_tasks.rb
+++ db/migrate/20200417022620_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
       t.string        :title
-      t.integer       :content
+      t.text       :content
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2020_04_17_022620) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.integer "content"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
